12914cd98cf743500adf8002789d6f631d35cc74
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-08-25 23:24:02 +0000

    I added some Routing Fuctions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 
 import React from 'react';
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-import HomePagesCards from './components/Cards/HomePagesCards';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Introduction from './components/Introduction/Introduction.component';
-import NavBarComponent from './components/Navbar/Navbar.component';
-import Programing from './Pages/Programing/Programing';
-import Student from './Pages/Student/Student';
-import Marketing from './Pages/Markeeting/Marketing';
-
+import NavBarComponent from './components/Navbar/Navbar.component'
+import Programing from './Pages/Programing/Programing'
+import Student from './Pages/Student/Student'
+import Marketing from './Pages/Markeeting/Marketing'
 
 function App() {
   return (
-		<div className="App">
-			<BrowserRouter>
-				<NavBarComponent />
-				<Routes>
-					<Route path="/ " element={<Introduction />} />
-					<Route path="/programing" element={<Programing />} />
-					<Route path="/Student" element={<Student />} />
-					<Route path="/Marketing" element={<Marketing />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+	<BrowserRouter>
+		<NavBarComponent />
+		<Routes>
+			<Route path="/" element={<Introduction />} />
+			<Route path="/programing" element={<Programing />} />
+			<Route path="/Student" element={<Student />} />
+			<Route path="/Marketing" element={<Marketing />} />
+		</Routes>
+	</BrowserRouter>
+  )
 }
 
 export default App;
