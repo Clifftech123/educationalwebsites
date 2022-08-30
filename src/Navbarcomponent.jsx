72cfd429310bc6c2f3d6e1eff462a85865f9d79e
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-08-30 19:02:41 +0000

    package.json file

--- src/Navbarcomponent.jsx
+++ src/Navbarcomponent.jsx
@@ -4,7 +4,7 @@ import {NavLink } from "react-router-dom";
 import { useState } from "react";
 import {FaGithub}  from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-import HomePagesCards from './HomePagesCards';
+
 
 
 
@@ -33,9 +33,8 @@ const NavBarComponent = () => {
 			<div className="      justify-between  px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 font">
 				<div>
 					<div className="flex items-center justify-between py-3 md:py-5 md:block">
-						<a href={HomePagesCards}>
 							<h2 className="text-2xl font-bold text-white">LOGO</h2>
-						</a>
+					
 						<div className="md:hidden">
 							<button
 								className="p-2 text-white rounded-md outline-none focus:border-gray-400 focus:border"
